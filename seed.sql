43b0433f3550eca23a803ea41d5db7c3bf25667c
SayedQassim <qassab300@gmail.com>
2024-11-07 11:08:00 +0300

    add seed.sql

---
+++ seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.family(
+    id, address, family_name)
+VALUES (0, "defult", "Unassined");
+
+INSERT INTO public.users(
+     email, password, phone_no, role, status, username, familyid)
+VALUES ("qassab300@gmail.com", "isbhfkjfnkasjfnkajfnasj", "123", "ADMIN", "ACTIVE", "qasim", 0);
