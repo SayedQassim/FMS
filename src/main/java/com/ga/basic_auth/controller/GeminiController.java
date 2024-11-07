281e0cc77a0badc367de01b811a5fbfe97159393
SayedQassim <qassab300@gmail.com>
2024-11-07 11:08:43 +0300

    disable ai service

--- src/main/java/com/ga/basic_auth/controller/GeminiController.java
+++ src/main/java/com/ga/basic_auth/controller/GeminiController.java
@@ -1,22 +1,22 @@
-package com.ga.basic_auth.controller;
-
-import com.ga.basic_auth.service.GeminiService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/ai")
-public class GeminiController {
-
-  @Autowired
-  private GeminiService geminiService;
-
-
-  @PostMapping("/generate")
-  public String generateText(@RequestBody String prompt) {
-    return geminiService.generateResponse(prompt);
-  }
-}
+//package com.ga.basic_auth.controller;
+//
+//import com.ga.basic_auth.service.GeminiService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/ai")
+//public class GeminiController {
+//
+//  @Autowired
+//  private GeminiService geminiService;
+//
+//
+//  @PostMapping("/generate")
+//  public String generateText(@RequestBody String prompt) {
+//    return geminiService.generateResponse(prompt);
+//  }
+//}
